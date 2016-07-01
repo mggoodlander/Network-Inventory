7b2893958ee0aa0b03b2218fc6ae5f9513273f64
mggoodlander <mggoodlander@outlook.com>
2016-07-01 15:55:05 -0500

    Add files via upload

--- NetOps/urls.py
+++ NetOps/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^Inventory/', include('Inventory.urls'))
+    url(r'^netops/', include('Inventory.urls'))
 ]
